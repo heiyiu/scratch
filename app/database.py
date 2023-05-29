8a1d55fe7a459732f0b87d5c90a8a5b01518bd9f
Hei Yiu <hyiubla@gmail.com>
2023-05-29 13:10:33 -0400

    remove hardcoded sql connection

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgres:5432/spay"
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+with open(os.environ.get("POSTGRES_PASSWORD_FILE")) as ppf:
+    POSTGRES_PASSWORD = ppf.read().strip()
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
