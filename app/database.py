566d68d94584df10e9d01fad7335a1e1ae237683
Hei Yiu <hyiubla@gmail.com>
2023-05-30 09:27:25 -0400

    support postgres pw env in k9s setting

--- app/database.py
+++ app/database.py
@@ -5,8 +5,11 @@ import os
 
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
-with open(os.environ.get("POSTGRES_PASSWORD_FILE")) as ppf:
-    POSTGRES_PASSWORD = ppf.read().strip()
+if os.environ.get("POSTGRES_PASSWORD"):
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+else:
+    with open(os.environ.get("POSTGRES_PASSWORD_FILE")) as ppf:
+        POSTGRES_PASSWORD = ppf.read().strip()
 SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:5432/{POSTGRES_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
